723fea90ac8bc677fc75bf03dbb9425db9783fb7
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2024-01-31 22:40:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "bucket1--30-1-2024" {
-  bucket = "agnija-bucket-terraform1-30-1-2024" # Update with your desired bucket name
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "agnija-bucket-terraform1" # Update with your desired bucket name
 }
 
-resource "aws_s3_bucket" "bucket2-30-1-2024" {
-  bucket = "agnija-bucket-terraform2-30-1-2024" # Update with your desired bucket name
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "agnija-bucket-terraform2" # Update with your desired bucket name
 }
 
-resource "aws_s3_bucket" "bucket3-30-1-2024" {
-  bucket = "agnija-bucket-terraform3-30-1-2024" # Update with your desired bucket name
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "agnija-bucket-terraform3" # Update with your desired bucket name
 }
